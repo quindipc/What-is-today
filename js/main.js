609eab382018069c46ec26cbff71ffe4d4ef33a4
Chelsea Quindipan <quindipc@gmail.com>
2022-02-22 17:56:07 -0500

    small edit

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ document.querySelector('#check').addEventListener('click', check)
 
 // FUNCTIONS 
 function check() {
-
   const day = document.querySelector('#day').value
-
-  // CONDITIONALS 
-  if (day === 'Tuesday') || (day === "Thursday") {
-    alert("IT'S STUDY TIME");
-  } else {
-    if (day === "Saturday") || (day === "Sunday")
-    alert("IT'S THE WEEKEND!!!");
-  } else {
-    if (day === undefined)
-      alert("WHAT ARE YOU DOING WITH YOUR LIFE TODAY!!!");
+    // CONDITIONALS 
+    if (day.toUpperCase() === "Tuesday") || (day.toLowerCase() === "Thursday") {
+     document.getElementById('#placeToSee').innerHTML = "Class day is today!!! Get ready to code!!";
+    } else if {
+      (day.toUpperCase() === "Saturday") || (day.toLowerCase() === "Sunday")
+      document.getElementById('#placeToSee').innerHTML = "It's the weekend let's party!";
+    } else {
+      document.getElementById('#placeToSee').innerHTML = "Class day is today!!! Get ready to code!!";
+    }
   }
+
+  
